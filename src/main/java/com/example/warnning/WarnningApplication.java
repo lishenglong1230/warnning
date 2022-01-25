e27ed869796b7d4735a33373f008533aec5f68bc
lwy123-wq <l2158223895@qq.com>
2022-01-25 14:58:51 +0800

    warn

---
+++ src/main/java/com/example/warnning/WarnningApplication.java
@@ -0,0 +1,13 @@
+package com.example.warnning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarnningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarnningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/warnning/WarnningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warnning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarnningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
