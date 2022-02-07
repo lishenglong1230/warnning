321738d610f5d600f7d2eaf824b740d0373f93c1
lxj <1873579697@qq.com>
2022-02-07 20:10:14 +0800

    类实体

---
+++ assess/src/main/java/com/example/assess/entity/login.java
@@ -0,0 +1,22 @@
+package com.example.assess.entity;
+
+public class login {
+    private String phone;
+    private String code;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ assess/src/main/java/com/example/assess/entity/oss.java
@@ -0,0 +1,40 @@
+package com.example.assess.entity;
+
+public class oss {
+    private String invoice;
+    private String certificate;
+    private String accident;
+    private String maintenance;
+
+    public String getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(String invoice) {
+        this.invoice = invoice;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getAccident() {
+        return accident;
+    }
+
+    public void setAccident(String accident) {
+        this.accident = accident;
+    }
+
+    public String getMaintenance() {
+        return maintenance;
+    }
+
+    public void setMaintenance(String maintenance) {
+        this.maintenance = maintenance;
+    }
+}
