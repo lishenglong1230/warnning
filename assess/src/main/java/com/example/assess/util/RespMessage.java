60c63c96887c8ab871d4202d950583401b513583
lxj <1873579697@qq.com>
2022-02-08 21:07:34 +0800

    sso

---
+++ assess/src/main/java/com/example/assess/util/RespMessage.java
@@ -0,0 +1,56 @@
+package com.example.assess.util;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * 接口响应对象
+ */
+public class RespMessage implements Serializable {
+
+    /**
+     * 响应编号
+     */
+    private String respCode;
+    /**
+     * 响应消息
+     */
+    private String respMsg;
+    /**
+     * 响应数据
+     */
+    private Map<String, Object> respArgs;
+
+    @Override
+    public String toString() {
+        return "RespMessage{" +
+                "respCode='" + respCode + '\'' +
+                ", respMsg='" + respMsg + '\'' +
+                ", respArgs=" + respArgs +
+                '}';
+    }
+
+    public String getRespCode() {
+        return respCode;
+    }
+
+    public void setRespCode(String respCode) {
+        this.respCode = respCode;
+    }
+
+    public String getRespMsg() {
+        return respMsg;
+    }
+
+    public void setRespMsg(String respMsg) {
+        this.respMsg = respMsg;
+    }
+
+    public Map<String, Object> getRespArgs() {
+        return respArgs;
+    }
+
+    public void setRespArgs(Map<String, Object> respArgs) {
+        this.respArgs = respArgs;
+    }
+}
---
+++ assess/src/main/java/com/example/assess/web/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.example.assess.web.controller;
+
+import com.example.assess.web.util.LoginCheck;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * SSO登录控制器
+ */
+@Controller
+@RequestMapping("/sso")
+public class LoginController {
+
+    /**
+     * 处理用户登录请求
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @param gotoUrl  登录成功后请求路径
+     */
+    @PostMapping("/doLogin")
+    public ModelAndView doLogin(String username, String password, String gotoUrl, HttpServletResponse response) {
+        ModelAndView mv = new ModelAndView("login_fail");
+        // 校验用户名和密码
+        boolean ok = LoginCheck.checkLogin(username, password);
+        // 判断是否登录成功
+        if (ok) {
+            Cookie cookie = new Cookie(LoginCheck.COOKIE_NAME, LoginCheck.COOKIE_VALUE);
+            // 顶级域名下，所有应用都是可见的
+            cookie.setPath("/");
+            // 添加Cookie
+            response.addCookie(cookie);
+            mv.setViewName("redirect:" + gotoUrl);
+        }
+        return mv;
+    }
+
+    /**
+     * 跳转到登录页面
+     */
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+
+}
