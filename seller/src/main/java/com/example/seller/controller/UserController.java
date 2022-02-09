2dccf95f5117d23627902516f9bea3b8c04a92f9
lwy123-wq <l2158223895@qq.com>
2022-02-09 19:00:51 +0800

    login

--- seller/src/main/java/com/example/seller/controller/UserController.java
+++ seller/src/main/java/com/example/seller/controller/UserController.java
@@ -1,8 +1,45 @@
 package com.example.seller.controller;
 
+import com.example.seller.entity.User;
+import com.example.seller.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Controller
 public class UserController {
-
+    @Autowired
+    private UserServiceImpl userService;
+    //登陆
+    public String login(HttpServletRequest request, HttpServletResponse response, String username, String password, Model model){
+        User user=userService.findByNameAndPassword(username, Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8)));
+        //System.out.println(user.getName()+user.getPasswd());
+        if(user.getUsername()==null||user.getPassword()==null){
+            return "error";
+        }else {
+            return "success";
+        }
+    }
+    //注册
+    @PostMapping(value = "/registry")
+    @ResponseBody
+    //@Cacheable(cacheNames = "register",key = "#username+'-'+#password+'-'+#email")
+    public String  register(String username,String password,String email){
+        User user =userService.findByName(username);
+        if(user.getUsername() == null){
+            //personService.register(id);
+            userService.insertUser(username, Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8)));
+            return "Y";
+        }
+        return "N";
+    }
 }
