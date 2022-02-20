c60cd8c4be731485ae9d49b2b4ce3938c5e2a836
lxj <1873579697@qq.com>
2022-02-20 21:33:29 +0800

    修改

--- assess/src/main/java/com/example/assess/controller/DemoOneController.java
+++ assess/src/main/java/com/example/assess/controller/DemoOneController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -44,22 +45,22 @@ public class DemoOneController {
     @ResponseBody
     public String demo1(HttpServletRequest request, HttpServletResponse response, String username, String password) {
         Map<String, String> map = CookieUtil.getCookies(request);
-        if (map.get(username)!=null){
-
+        CodeUtil utilCode=new CodeUtil();
+        s = utilCode.verifyCode();
+        int expire = 60 * 60 * 24 * 20;  //表示7天
+        Login login = registerService.FindNamePasswd(username, password);
+        String k = map.get("username");
+        if (k!=null){
             return "success";
         }
    /*     Map<String, String> cookies = CookieUtil.getCookies(request);
         for (int i=0;i<=cookies.size();i++){
             System.out.println(cookies.get(i));
         }*/
-        CodeUtil utilCode=new CodeUtil();
-         s = utilCode.verifyCode();
-        int expire = 60 * 60 * 24 * 20;  //表示7天
-        Login login = registerService.FindNamePasswd(username, password);
         if (login.getUsername()==null||login.getPassword()==null){
             return "error";
         }else {
-            if (!loginCheck.checkCookie(request,s)){
+            if (!loginCheck.checkCookie(request, this.s)){
                 CookieUtil.setCookie(request, response, "username", login.getUsername(), expire);
                 CookieUtil.setCookie(request, response, "password", login.getPassword(), expire);
                 return "success";
